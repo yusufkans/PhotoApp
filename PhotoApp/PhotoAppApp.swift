3d44e98267bb906b6b784898bd1f3bbea741915c
Yusufkan Sürmelioğlu <yusufkan.surmelioglu@vestel.com.tr>
2024-08-11 11:18:39 +0300

    Initial commit

---
+++ PhotoApp/PhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppApp.swift
+//  PhotoApp
+//
+//  Created by Yusufkan Sürmelioğlu on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
